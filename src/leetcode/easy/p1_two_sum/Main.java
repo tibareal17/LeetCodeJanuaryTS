81e805bcd60c694b76e589d3fff70ba2b240d385
Timur Bagdat <takoihero17@gmail.com>
2026-01-16 11:35:54 +0500

    Initial commit: Two Sum solution

---
+++ src/leetcode/easy/p1_two_sum/Main.java
@@ -0,0 +1,13 @@
+package leetcode.easy.p1_two_sum;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(Arrays.toString(sol.twoSum(new int[]{2, 7, 11, 15}, 17)));
+
+
+    }
+}
