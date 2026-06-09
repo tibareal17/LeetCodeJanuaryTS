93d8be6ba0f2d3bbc2b4547d33edb0cafa8d6b1c
Timur Bagdat <takoihero17@gmail.com>
2026-06-09 17:24:03 +0500

    check git

--- src/leetcode/easy/p0088_merge_sorted_array/Main.java
+++ src/leetcode/easy/p0088_merge_sorted_array/Main.java
@@ -1,4 +1,13 @@
 package leetcode.easy.p0088_merge_sorted_array;
 
 public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums1 = {0,1,2,3,0,0,0};
+        int m = 4;
+        int[] nums2 = {1,2,2};
+        int n = nums2.length;
+        solution.merge(nums1, m, nums2, n);
+    }
 }
--- src/leetcode/easy/p0088_merge_sorted_array/Solution.java
+++ src/leetcode/easy/p0088_merge_sorted_array/Solution.java
@@ -1,4 +1,28 @@
 package leetcode.easy.p0088_merge_sorted_array;
 
 public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = 0;
+
+        int[] temp = new int[nums1.length];
+
+        int i = 0;
+        int j = 0;
+        while (k != nums1.length - 1){
+            if (nums1[i] <= nums2[j]){
+                temp[k] = nums1[i];
+                i++;
+            }else{
+                temp[k] = nums2[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        for (int l = 0; l < nums1.length; l++) {
+            nums1[l] = temp[l];
+            System.out.println(temp[l]);
+        }
+    }
 }
