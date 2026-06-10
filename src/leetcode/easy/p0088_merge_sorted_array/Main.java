a680383e1ca3c6cd6b885c10cbda1f876b2d8d64
Timur Bagdat <takoihero17@gmail.com>
2026-06-10 14:34:52 +0500

    Solve LeetCode 88: Merge Sorted Array

--- src/leetcode/easy/p0088_merge_sorted_array/Main.java
+++ src/leetcode/easy/p0088_merge_sorted_array/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[] nums1 = {0,1,2,3,0,0,0};
+        int[] nums1 = {2,2,4,5,0,0,0};
         int m = 4;
-        int[] nums2 = {1,2,2};
+        int[] nums2 = {1,3,6};
         int n = nums2.length;
         solution.merge(nums1, m, nums2, n);
     }
--- src/leetcode/easy/p0088_merge_sorted_array/Solution.java
+++ src/leetcode/easy/p0088_merge_sorted_array/Solution.java
@@ -2,27 +2,38 @@ package leetcode.easy.p0088_merge_sorted_array;
 
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int k = 0;
+        int k = nums1.length - 1;
 
-        int[] temp = new int[nums1.length];
-
-        int i = 0;
-        int j = 0;
-        while (k != nums1.length - 1){
-            if (nums1[i] <= nums2[j]){
-                temp[k] = nums1[i];
-                i++;
-            }else{
-                temp[k] = nums2[j];
-                j++;
+        if (nums2.length == 0){
+            return;
+        }else if (m == 0){
+            for (int i = 0; i < nums1.length; i++) {
+                nums1[i] = nums2[i];
             }
-
-            k++;
+            return;
         }
 
-        for (int l = 0; l < nums1.length; l++) {
-            nums1[l] = temp[l];
-            System.out.println(temp[l]);
+        int j = n - 1;
+
+        // O(n+m)
+        for (int i = nums1.length - n - 1; i >= 0 ; i--) {
+            while (j >= 0){
+                if (nums1[i] >= nums2[j]){
+                    nums1[k] = nums1[i];
+                    k--;
+                    break;
+                }
+
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+        }
+
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 }
