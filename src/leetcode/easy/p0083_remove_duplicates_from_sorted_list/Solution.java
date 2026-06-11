761fe5b6a885f38902e69667527a309b1cdc358b
Timur Bagdat <takoihero17@gmail.com>
2026-06-11 17:28:51 +0500

    started 83 task

---
+++ src/leetcode/easy/p0083_remove_duplicates_from_sorted_list/Solution.java
@@ -0,0 +1,9 @@
+package leetcode.easy.p0083_remove_duplicates_from_sorted_list;
+
+import leetcode.easy.p0206_reverse_linked_list.ListNode;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+    }
+}
