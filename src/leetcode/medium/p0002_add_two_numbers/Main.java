2b598b648f43536bd0023bca8a39111406424fb8
Timur Bagdat <takoihero17@gmail.com>
2026-06-18 17:32:04 +0500

    will continue 2nd task later

---
+++ src/leetcode/medium/p0002_add_two_numbers/Main.java
@@ -0,0 +1,7 @@
+package leetcode.medium.p0002_add_two_numbers;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/leetcode/medium/p0002_add_two_numbers/Solution.java
+++ src/leetcode/medium/p0002_add_two_numbers/Solution.java
@@ -5,5 +5,40 @@ import leetcode.easy.p0206_reverse_linked_list.ListNode;
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
+        int digit = 1;
+        int sumOne = 0;
+        while (l1 != null) {
+            sumOne += l1.val * digit;
+            l1 = l1.next;
+            digit *= 10;
+        }
+        digit = 1;
+
+        int sumTwo = 0;
+        while (l2 != null) {
+            sumTwo += l2.val * digit;
+            l2 = l2.next;
+            digit *= 10;
+        }
+        digit = 1;
+
+        int total = sumOne + sumTwo;
+
+        ListNode res = new ListNode();
+        ListNode head = res;
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (total != 0) {
+            res.val = total % 10;
+            total /= 10;
+            res.next = new ListNode();
+            res = res.next;
+
+        }
+
+
+        return head;
     }
 }
